a2f01df077a5bfe77547113a42908ae450a2df4d
Priyanka Malviya <priyanka_malviya@icloud.com>
2016-08-01 23:12:52 -0700

    adding contacts using forms

--- reactContacts/src/ContactsList.js
+++ reactContacts/src/ContactsList.js
@@ -3,18 +3,29 @@ import Contact from './Contact';
 import {render} from 'react-dom';
 
 class ContactsList extends React.Component {
-    constructor(){
-        super();
+    constructor(props){
+        super(props);
         this.state = {
-            search:''
+            search:'',
+            contacts: props.contacts
         };
     }
 
     updateSearch(event) {
         this.setState({search:event.target.value.substring(0,20)});
     }
+
+    addContact(event){
+        event.preventDefault();
+        let name = this.refs.name.value;
+        let phone = this.refs.phone.value;
+        let id=Math.floor(Math.random()*100 +1);
+        this.setState({
+            contacts: this.state.contacts.concat({id, name, phone})
+        })
+    }
     render(){
-        let filteredContacts = this.props.contacts.filter(
+        let filteredContacts = this.state.contacts.filter(
             (contact) =>{
                 return contact.name.toLowerCase().indexOf(this.state.search.toLowerCase()) !== -1;
             }
@@ -22,8 +33,14 @@ class ContactsList extends React.Component {
         return (
             <div>
                 <input type="text"
+                       placeholder="Search"
                        value={this.state.search}
-                       onChange={this.updateSearch.bind(this)}/>
+                       onChange={this.updateSearch.bind(this)} />
+                <form onSubmit={this.addContact.bind(this)}>
+                    <input type="text" ref="name"/>
+                    <input type="text" ref="phone"/>
+                    <button type="submit">Add New Contact</button>
+                </form>
                 <ul>
                     {
                         filteredContacts.map((contact)=> {
