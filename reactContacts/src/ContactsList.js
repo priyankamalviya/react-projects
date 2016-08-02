8a3c04b4cba3e882817779408e2891d3008b2dff
Priyanka Malviya <priyanka_malviya@icloud.com>
2016-08-01 22:47:22 -0700

    adding a search interface

--- reactContacts/src/ContactsList.js
+++ reactContacts/src/ContactsList.js
@@ -3,15 +3,35 @@ import Contact from './Contact';
 import {render} from 'react-dom';
 
 class ContactsList extends React.Component {
+    constructor(){
+        super();
+        this.state = {
+            search:''
+        };
+    }
+
+    updateSearch(event) {
+        this.setState({search:event.target.value.substring(0,20)});
+    }
     render(){
+        let filteredContacts = this.props.contacts.filter(
+            (contact) =>{
+                return contact.name.toLowerCase().indexOf(this.state.search.toLowerCase()) !== -1;
+            }
+        );
         return (
+            <div>
+                <input type="text"
+                       value={this.state.search}
+                       onChange={this.updateSearch.bind(this)}/>
                 <ul>
                     {
-                        this.props.contacts.map((contact)=> {
+                        filteredContacts.map((contact)=> {
                             return   <Contact contact ={contact} key={contact.id}/>
                         })
                     }
                 </ul>
+            </div>
         )
     }
 }
